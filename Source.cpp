dfe3cf671f42792e064d28cf811d4f40d2335244
Kory smith <kosa.smit@gmail.com>
2019-04-15 22:07:33 +1000

    [fixed] debug and result console logs

--- Source.cpp
+++ Source.cpp
@@ -5,41 +5,43 @@
 #include <chrono>
 #include <string>
 
-const int ARRAY_SIZE = 500;
-const int MAX_VALUE = 1000;
+const int ARRAY_SIZE = 1000000;
+const int MAX_VALUE = 1000000;
+
 
 /// typedefs for easy type creation
 using TIME_REGISTER = std::map <std::string, std::chrono::time_point<std::chrono::steady_clock>>;
 using TIME_VECTOR_PAIR = std::pair<std::string, long long>;
 using TIME_VECTOR = std::vector<TIME_VECTOR_PAIR>;
 
-TIME_REGISTER m_beginTimerList;
-TIME_REGISTER m_finishTimerList;
-TIME_VECTOR m_finalTimerSheetMs;
+std::shared_ptr<TIME_REGISTER> m_beginTimerList = std::make_shared<TIME_REGISTER>();
+std::shared_ptr<TIME_REGISTER> m_finishTimerList = std::make_shared<TIME_REGISTER>();
+std::shared_ptr<TIME_VECTOR> m_finalTimerSheetMs = std::make_shared<TIME_VECTOR>();
 
 // log start timer
 void addStartTime(std::string displayName)
 {
 	auto startTimer = std::chrono::high_resolution_clock::now();
-	m_beginTimerList.insert(std::make_pair(displayName, startTimer));
-	m_finalTimerSheetMs.push_back(TIME_VECTOR_PAIR(displayName, 0));
+	m_beginTimerList->insert(std::make_pair(displayName, startTimer));
+	m_finalTimerSheetMs->push_back(TIME_VECTOR_PAIR(displayName, 0));
 };
 // lof finish timer
 void addFinishTime(std::string id)
 {
 	//log finish timer
 	auto finishTimer = std::chrono::high_resolution_clock::now();
-	m_finishTimerList.insert(std::make_pair(id, finishTimer));
+	m_finishTimerList->insert(std::make_pair(id, finishTimer));
 };
 // print time sheet of finihsed timers
 void printFinalTimeSheet()
 {
 	// iterate through all values in timesheet and print them.
-	for (auto obj : m_finalTimerSheetMs)
+	for (auto & obj : *m_finalTimerSheetMs)
 	{
-		auto differenceInTime = m_finishTimerList[obj.first] - m_beginTimerList[obj.first];
+		obj.second - obj.second;
+		auto differenceInTime = m_finishTimerList->at(obj.first) - m_beginTimerList->at(obj.first);
 		obj.second = std::chrono::duration_cast<std::chrono::milliseconds>(differenceInTime).count();
-		std::cout << obj.first << " : " << std::to_string(obj.second) << "ms to execute\n" << std::endl;
+		std::cout << obj.first << " : " << std::to_string(obj.second) << "ms / " << std::to_string(float(float(obj.second)/1000.f)) <<"sec to execute\n" << std::endl;
 	}
 }
 
@@ -106,9 +108,11 @@ int main() {
 		array.push_back((rand() % MAX_VALUE) + 1);
 	}
 	int arraySize = array.size();
+
+	std::cout << "Array size: " << std::to_string(ARRAY_SIZE) << std::endl;
+	std::cout << "Values: [1 - " << std::to_string(MAX_VALUE) << "]" << std::endl;
 	
 	addStartTime("Quicksort Elapsed: ");
-
 	quicksort(array, 0, arraySize - 1, arraySize);
 	addFinishTime("Quicksort Elapsed: ");
 	
